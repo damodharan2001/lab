9ada61cd796cd1b482dd5bc510fea8ecf0bed8d0
unknown <dharandamo007@gmail.com>
2022-05-06 22:34:31 +0530

    Addition of two numbers

---
+++ add.py
@@ -0,0 +1,6 @@
+print("Enter two numbers for addition")
+print("Enter the first number : ")
+num1 = int(input())
+print("Enter the second number : ")
+num2 = int(input())
+print("The result ", num1+num2)
